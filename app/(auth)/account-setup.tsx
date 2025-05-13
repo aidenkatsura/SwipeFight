730ea728f56091989bd5c583c46912bbedb33d31
YashveerP <yashveer@cs.washington.edu>
2025-05-13 14:03:06 -0700

    added likes and dislikes arrays on account creation

--- app/(auth)/account-setup.tsx
+++ app/(auth)/account-setup.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import { router } from 'expo-router';
 import { theme } from '@/styles/theme';
 import { auth, db } from '@/FirebaseConfig';
-import { doc, setDoc } from 'firebase/firestore';
+import { doc, setDoc, Timestamp } from 'firebase/firestore';
 import { Discipline } from '@/types/fighter';
 import * as ImagePicker from 'expo-image-picker';
 
@@ -89,7 +89,10 @@ export default function AccountSetupScreen() {
         wins: 0,
         losses: 0,
         draws: 0,
-        createdAt: new Date().toISOString(),
+        likes: [],
+        dislikes: [],
+        chats: [],
+        createdAt: Timestamp.fromDate(new Date()),
       });
 
       console.log('Profile created successfully, navigating to tabs');
