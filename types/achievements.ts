1cb8ea8afd3350efa005f9641df89fc982bb5f1d
Aiden Katsuragawa <aiden6905@gmail.com>
2025-05-23 11:36:39 -0700

    created achievements

---
+++ types/achievements.ts
@@ -0,0 +1,24 @@
+export const ACHIEVEMENTS = {
+  wins: [
+    { count: 1, name: "Victory: Reach 1 Win"},
+    { count: 10, name: "Rising Fighter: Reach 10 Wins"},
+    { count: 25, name: "Veteran Warrior: Reach 25 Wins"},
+    { count: 50, name: "Champion: Reach 50 Wins"}
+  ],
+  losses: [
+    { count: 1, name: "Defeat: Reach 1 Loss"},
+    { count: 10, name: "Resiliant Fighter: Reach 10 Losses"},
+    { count: 25, name: "Never Back Down: Reach 25 Losses"}
+  ],
+  draws: [
+    { count: 1, name: "Stalemate: Reach 1 Draw"},
+    { count: 10, name: "Equalizer: Reach 10 Draws"},
+    { count: 25, name: "Perfectly Balanced: Reach 25 Draws"},
+  ],
+  rating: [
+    { count: 0, name: "Rock Bottom: Reach 0 Rating"},
+    { count: 900, name: "New Low: Reach 900 Rating"},
+    { count: 1100, name: "Proven Fighter: Reach 1100 Rating" },
+    { count: 1500, name: "Top of the Mountain: Reach 1500 Rating" },
+  ]
+};
